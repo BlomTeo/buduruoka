7b70a672651274d9538e967ef338cf7160822a77
Teo Blomqvist <teoblomqvist@Teo-MacBook-Air.local>
2026-09-05 19:34:20 +0300

    Save recipes to database

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
-
+from flask import request
+import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,4 +10,27 @@ def index():
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
+
+    if request.method == "POST":
+        title = request.form["title"]
+        ingredients = request.form["ingredients"]
+        instructions = request.form["instructions"]
+        price = request.form["price"]
+        servings = request.form["servings"]
+
+        connection = sqlite3.connect("database.db")
+
+        connection.execute(
+            """
+            INSERT INTO recipes
+            (title, ingredients, instructions, price, servings)
+            VALUES (?, ?, ?, ?, ?)
+            """,
+            (title, ingredients, instructions, price, servings)
+        )
+
+        connection.commit()
+        connection.close()
+
     return render_template("add.html")
+
