632524fd43adf5fe6ef14e1e108441ac1e7f286b
Teo Blomqvist <teoblomqvist@Teo-MacBook-Air.local>
2026-09-06 16:14:05 +0300

    Move database setup to schema.sql

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE recipes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    ingredients TEXT NOT NULL,
+    instructions TEXT NOT NULL,
+    price REAL,
+    servings INTEGER
+);
