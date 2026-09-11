1ebc5102932d793df34035b67c3a282498e63b83
Teo Blomqvist <teoblomqvist@Teo-MacBook-Air.local>
2026-09-11 17:07:56 +0300

    Add users table

--- schema.sql
+++ schema.sql
@@ -6,3 +6,9 @@ CREATE TABLE recipes (
     price REAL,
     servings INTEGER
 );
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL
+);
